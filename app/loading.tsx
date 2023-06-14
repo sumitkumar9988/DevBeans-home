1b185c55256bc11e102ac3bb377222bc4e3191fb
sumit kumar <sumi9998877@gmail.com>
2023-06-14 23:25:59 +0530

    add waitlist

--- app/loading.tsx
+++
@@ -1,5 +0,0 @@
-import React, { useEffect } from "react";
-
-export default function loading() {
-  return <div>loading...</div>;
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 /* eslint-disable react/jsx-key */
-import demo from "./../data/demo.json";
-import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState, ChangeEvent } from "react";
 import Lottie from "lottie-react";
 import Header from "../components/headers";
+import demo from "./../data/demo.json";
+
+const supabaseUrl: string = "https://dnflmwpentrmpauuzrns.supabase.co";
+const supabaseKey: string =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuZmxtd3BlbnRybXBhdXV6cm5zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4Njc0MTk4MiwiZXhwIjoyMDAyMzE3OTgyfQ.fm2wKs7nCUreFmT9w-aiA5IEEsdTs8R4y7Me5izflxg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 const copy = {
   heading: "Simple & Fast Figma to React Component",
   subHeading:
@@ -43,6 +50,22 @@ const copy = {
 export default function Home() {
   const [animationClass, setAnimationClass] = useState("");
   const [randomPath, setRandomPath] = useState("");
+  const [email, setEmail] = useState("");
+
+  console.log(email);
+
+  const submitEmail = async () => {
+    console.log("email", email);
+    let { data, error } = await supabase
+      .from("Waitlist")
+      .insert([{ email: email }]);
+
+    if (error) {
+      setEmail("");
+    } else {
+      setEmail("");
+    }
+  };
 
   useEffect(() => {
     const paths = [
@@ -109,8 +132,7 @@ export default function Home() {
                   className="w-8 h-8 inline-block stroke-[]"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinejoin="round"
                     d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
                   />
                 </svg>
@@ -135,16 +157,19 @@ export default function Home() {
               className="text-2xl lg:text-5xl font-normal font-sans placeholder-gray-200 focus:outline-none"
               placeholder="Enter Email"
               type="email"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             ></input>
             <div className="flex flex-row items-start justify-center  gap-8 ">
-              <a
-                href="https://www.figma.com/community/plugin/1197376201235364148/Figma-to-HTML%2FCSS-Code"
+              <div
+                onClick={submitEmail}
                 className="relative inline-flex items-center justify-center hover:bg-gray-700 bg-black transition ease-in-out delay-150 duration-200  font-sans px-4 lg:px-12 py-2 lg:py-3 overflow-hidden font-semibold   rounded-full  group  cursor-pointer"
               >
                 <span className="relative text-white text-sm  font-semibold font-sans">
                   Join our Newsletter
                 </span>
-              </a>
+              </div>
             </div>
           </div>
         </div>
